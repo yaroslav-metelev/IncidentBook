3b4ff3941daee1a00f1b07a8c52e0b93b8039f02
Ubivator2009 <Jeos@bk.ru>
2025-01-25 19:58:42 +0700

    Added CORS

--- Program.cs
+++ Program.cs
@@ -8,6 +8,15 @@ builder.Services.AddDbContext<IncidentContext>(opt =>
     opt.UseInMemoryDatabase("IncidentList"));
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddCors(options =>
+{
+    options.AddPolicy("AllowAll", builder =>
+    {
+        builder.AllowAnyOrigin()
+               .AllowAnyMethod()
+               .AllowAnyHeader();
+    });
+});
 
 var app = builder.Build();
 
@@ -22,5 +31,5 @@ app.UseHttpsRedirection();
 app.UseAuthorization();
 
 app.MapControllers();
-
+app.UseCors("AllowAll");
 app.Run();
--- obj/Debug/net8.0/IncidentBook.AssemblyInfo.cs
+++ obj/Debug/net8.0/IncidentBook.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("IncidentBook")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f0bf729a6854c24852137979340e45f4bbee99e2")]
 [assembly: System.Reflection.AssemblyProductAttribute("IncidentBook")]
 [assembly: System.Reflection.AssemblyTitleAttribute("IncidentBook")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
